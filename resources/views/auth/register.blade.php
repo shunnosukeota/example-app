cbd2d5567775f5fb5dfae684eea73817469dcf49
shunnosukeota <113609549+shunnosukeota@users.noreply.github.com>
2022-11-16 20:12:13 +0900

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,5 +60,9 @@
                 </x-primary-button>
             </div>
         </form>
+         <div class="flex items-center justify-end mt-4">
+                <p class="underline text-sm text-gray-600 hover:text-gray-900">
+                    {{ __('エラーの場合は再度読み込みをしてください。') }}
+                </p>
     </x-auth-card>
 </x-guest-layout>
